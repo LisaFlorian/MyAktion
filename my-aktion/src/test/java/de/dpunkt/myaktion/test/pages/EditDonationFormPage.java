84d21a9f4c81d2967e9d2338356afab1eb695a03
FlorianL <FlorianL@HP112.statistik.rlp.de>
2017-11-23 15:34:56 +0100

    Tests

---
+++ my-aktion/src/test/java/de/dpunkt/myaktion/test/pages/EditDonationFormPage.java
@@ -0,0 +1,19 @@
+package de.dpunkt.myaktion.test.pages;
+
+import org.jboss.arquillian.graphene.page.Location;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+@Location("editDonationForm.sf")
+public class EditDonationFormPage extends AbstractPage {
+	@FindBy(xpath = "//textarea[contains(@id, 'url')]")
+	private WebElement formUrl;
+	
+	public void assertOnPage() {
+		assertTitle("editDonationForm.edit_donation_form");
+	}
+	
+	public String getFormURL() {
+		return formUrl.getText();
+	}
+}
